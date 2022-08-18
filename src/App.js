3b8e29003633774547c6a69c2ccea8cf0bbc5f3f
subasree <subasreebs5@gmail.com>
2022-08-18 10:33:51 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
 import ProductsFilters from "./ProductsFilters";
-// import DataTable from "./DemoTable";
-import SelectTableComponent from "./DemoTable2";
 
 const App = () => {
   return (
     <div className="App">
       <ProductsFilters />
-      {/* <DataTable /> */}
-      {/* <SelectTableComponent /> */}
       <br />
     </div>
   );
